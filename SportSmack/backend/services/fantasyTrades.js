04623b5e3dbc3fab52a55ca6f5c4cbcbed1f8dc2
torohr99 <tor99runner@gmail.com>
2026-09-06 10:04:34 -0600

    Update fantasyTrades.js

--- SportSmack/backend/services/fantasyTrades.js
+++ SportSmack/backend/services/fantasyTrades.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const prisma = require('../lib/prisma');
 
 /**
  * Verify that a player is currently owned by a specific fantasy team.
