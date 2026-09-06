d7986e05aa0225af01cf1e6e68681e93d8888db3
torohr99 <tor99runner@gmail.com>
2026-09-06 09:42:34 -0600

    Refactor Prisma client import in posts route

--- SportSmack/backend/routes/posts.js
+++ SportSmack/backend/routes/posts.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
 const authMiddleware = require('../middleware/auth');
 
-const prisma = new PrismaClient();
+const prisma = require('../lib/prisma');
 
 // Protect all post routes
 router.use(authMiddleware);
