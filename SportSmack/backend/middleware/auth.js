467f1f1218132b42194430dd377bed7c76fbcd85
torohr99 <tor99runner@gmail.com>
2026-09-08 09:31:00 -0600

    Update auth.js

--- SportSmack/backend/middleware/auth.js
+++ SportSmack/backend/middleware/auth.js
@@ -4,10 +4,6 @@ const authMiddleware = (req, res, next) => {
   // Get token from cookie
   let token = req.cookies ? req.cookies.smack_auth : null;
   
-  if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-    token = req.headers.authorization.split(' ')[1];
-  }
-
   if (!token) {
     return res.status(401).json({ message: 'Not authorized, no token' });
   }
