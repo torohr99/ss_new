9ce37db84897324d56fc85b5c8f59b9c84e98b1d
torohr99 <tor99runner@gmail.com>
2026-09-10 06:44:36 -0600

    Create loading.js

---
+++ SportSmack/frontend/app/loading.js
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <main
+      style={{
+        minHeight: '50vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <p>
+        Loading SportSmack...
+      </p>
+    </main>
+  );
+}
