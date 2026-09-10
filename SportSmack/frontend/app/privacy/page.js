1c423157503edfc0dc1809f153f4b3d09c189d4c
torohr99 <tor99runner@gmail.com>
2026-09-10 06:51:15 -0600

    Create page.js

---
+++ SportSmack/frontend/app/privacy/page.js
@@ -0,0 +1,12 @@
+export default function PrivacyPage() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        SportSmack privacy policy
+        coming soon.
+      </p>
+    </main>
+  );
+}
