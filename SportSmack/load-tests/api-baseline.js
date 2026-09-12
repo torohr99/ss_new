249ba49c1b765a3e89c2778debe914ea343d2f75
torohr99 <tor99runner@gmail.com>
2026-09-12 12:30:20 -0600

    Update api-baseline.js

--- SportSmack/load-tests/api-baseline.js
+++ SportSmack/load-tests/api-baseline.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 25,
+  vus: 50,
   duration: '30s',
   thresholds: {
     http_req_failed: ['rate<0.01'],
