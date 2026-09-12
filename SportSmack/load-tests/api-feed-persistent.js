b3c6d3fe4b7f463c50297b37def34e4a4e46e3dd
torohr99 <tor99runner@gmail.com>
2026-09-12 14:39:54 -0600

    Update api-feed-persistent.js

--- SportSmack/load-tests/api-feed-persistent.js
+++ SportSmack/load-tests/api-feed-persistent.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 5,
+  vus: 10,
   duration: '30s',
 
   // Keep each VU's cookies between iterations.
@@ -10,7 +10,7 @@ export const options = {
 
   thresholds: {
     http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(95)<1000']
+    http_req_duration: ['p(95)<2000']
   }
 };
 
