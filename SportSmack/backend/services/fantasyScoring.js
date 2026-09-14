9044026246280b1671a8c9e5264b86fe08471c28
torohr99 <tor99runner@gmail.com>
2026-09-14 13:52:43 -0600

    Update fantasyScoring.js

--- SportSmack/backend/services/fantasyScoring.js
+++ SportSmack/backend/services/fantasyScoring.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../lib/prisma');
 
 // PPR Scoring Settings
 const SCORING = {
